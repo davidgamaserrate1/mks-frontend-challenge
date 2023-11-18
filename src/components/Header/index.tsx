0f3f67926ff449009120ff3e18ced370f44a24dc
David Gama Serrate <david.serrate@grupocard.com.br>
2023-11-18 18:16:33 -0300

    progress: adicionar componente header com styled components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+const HeaderStyles = styled.div`
+display: flex;
+justify-content: center;
+align-items:center ;
+background-color: #2D7AFA;
+height: 88px;
+`
+
+
+const Header = ()=>{
+    return(
+        <HeaderStyles>
+            Header
+        </HeaderStyles>
+         
+    )
+}
+
+export default Header
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,18 @@
 import axios from 'axios'
 import {useQuery} from 'react-query'
 import { Product } from '../types'
+import Header from '../components/Header'
 
 const Home = ()=>{
     const products_api = process.env.REACT_APP_PRODUCTS_API_URL || ''
-    const { data, isLoading, error } = useQuery("products", ()=>{
-        return axios
-            .get(products_api.toString())
-            .then((response)=>response.data)
-    },{
-        retry: 3
-    })
+    const { data, isLoading, error } = useQuery("products", 
+        ()=>{
+            return axios
+                .get(products_api.toString())
+                .then((response)=>response.data)
+            },{
+                retry: 3
+        })
     
     if(isLoading){
         return(
@@ -30,6 +32,7 @@ const Home = ()=>{
 
     return(
         <>
+            <Header />
              {data.products.map((product:Product) =>
              <div> {product.name} </div>
              )}
