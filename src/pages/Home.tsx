5bfcc1f8687631dc5f2a02706b719bdd9d8052c4
David Gama Serrate <david.serrate@grupocard.com.br>
2023-11-18 18:08:54 -0300

    feat: adicionar tipo Produto e versão inicial da utilização da api

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
-
+import axios from 'axios'
+import {useQuery} from 'react-query'
+import { Product } from '../types'
 
 const Home = ()=>{
+    const products_api = process.env.REACT_APP_PRODUCTS_API_URL || ''
+    const { data, isLoading, error } = useQuery("products", ()=>{
+        return axios
+            .get(products_api.toString())
+            .then((response)=>response.data)
+    },{
+        retry: 3
+    })
+    
+    if(isLoading){
+        return(
+            <div>
+                Carregando...
+            </div>
+        )
+    }
+
+    if(error){
+        return(
+            <div>
+                Algo deu errado
+            </div>
+        )
+    }
+
     return(
         <>
-            Home
+             {data.products.map((product:Product) =>
+             <div> {product.name} </div>
+             )}
         </>
     )
 }
---
+++ src/types.ts
@@ -0,0 +1,10 @@
+
+export interface Product {
+    id: number;
+    name: string;
+    brand: string;
+    description: string;
+    photo: string;
+    price: number;
+  }
+  
